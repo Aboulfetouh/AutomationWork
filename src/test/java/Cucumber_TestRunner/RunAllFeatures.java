99a140ac8c71ad1221fe1901e212aad81d25ba8e
EJADACAIRO\mabdelaziz <mkmahmoud@ejada.com>
2023-01-03 13:13:53 +0200

    AllureReports commit

--- src/test/java/Cucumber_TestRunner/RunAllFeatures.java
+++ src/test/java/Cucumber_TestRunner/RunAllFeatures.java
@@ -4,6 +4,6 @@ import OrangehrmLive_Pageclass.baseclass;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Cucumber_Features", glue = { "Cucumber_StepDefinitions" }, plugin = {
-		"pretty", "html:target/cucumber-reports.html" })
+		"pretty", "html:target/cucumber-reports.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 public class RunAllFeatures extends baseclass {
 }
--- src/test/java/Cucumber_TestRunner/parallelRun.java
+++ src/test/java/Cucumber_TestRunner/parallelRun.java
@@ -6,7 +6,7 @@ import OrangehrmLive_Pageclass.baseclass;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/Cucumber_Features", glue = { "Cucumber_StepDefinitions" }, plugin = {
-		"pretty", "html:target/cucumber-reports.html" })
+		"pretty", "html:target/cucumber-reports.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
 
 public class parallelRun extends baseclass {
 
